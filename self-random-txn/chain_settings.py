f01e05c589dd99749ce0436c76d049fee2feb29c
AndreyTryastsin879 <59559469+AndreyTryastsin879@users.noreply.github.com>
2023-04-10 08:37:49 +0300

    Add files via upload

---
+++ self-random-txn/chain_settings.py
@@ -0,0 +1,87 @@
+polygon = {
+            'name': 'Polygon',
+            'url':'https://polygon.llamarpc.com',
+            'min_amount_for_sending':0.0001,
+            'max_amount_for_sending':0.001,
+            'min_transactions':5,
+            'max_transactions':10,
+            'explorer_url':'https://polygonscan.com/',
+            'explorer_txn_path':'tx',
+            }
+
+
+bsc = {
+            'name': 'Binance Smart Chain',
+            'url':'https://bsc.publicnode.com',
+            'min_amount_for_sending':0.0001,
+            'max_amount_for_sending':0.001,
+            'min_transactions':5,
+            'max_transactions':10,
+            'explorer_url':'https://bscscan.com/',
+            'explorer_txn_path':'tx',
+            }
+
+
+avalanche = {
+            'name': 'Avalanche',
+            'url':'https://avalanche-c-chain.publicnode.com',
+            'min_amount_for_sending':0.0001,
+            'max_amount_for_sending':0.001,
+            'min_transactions':5,
+            'max_transactions':10,
+            'explorer_url':'https://snowtrace.io/',
+            'explorer_txn_path':'tx',
+            }
+
+
+__all__ = ['polygon', 'bsc', 'avalanche']
+	
+
+#template = {
+#            'name':'',
+#            'symbol':'',
+#            'url':'',
+#            'min_value_for_sending':'',
+#            'max_value_for_sending':'',
+#            'explorer_url':'',
+#            'explorer_txn_path':'tx',
+#            }
+
+
+#polygon_testnet = {
+#            'name': 'Polygon',
+#            'url':'https://endpoints.omniatech.io/v1/matic/mumbai/public',
+#            'min_amount_for_sending':0.0001,
+#            'max_amount_for_sending':0.001,
+#            'min_transactions':5,
+#            'max_transactions':10,
+#            'explorer_url':'https://mumbai.polygonscan.com',
+#            'explorer_txn_path':'tx',
+#            }
+#
+#
+#bsc_testnet = {
+#        'name':'Binance Smart Chain',
+#        'url':'https://bsc-testnet.public.blastapi.io',
+#        'min_amount_for_sending':0.0001,
+#        'max_amount_for_sending':0.001,
+#        'min_transactions':5,
+#        'max_transactions':10,
+#        'explorer_url':'https://testnet.bscscan.com/',
+#        'explorer_txn_path':'tx',
+#        }
+#
+#
+#avalanche_testnet = {
+#            'name':'Avalanche',
+#            'url':'https://endpoints.omniatech.io/v1/avax/fuji/public',
+#            'min_amount_for_sending':0.0001,
+#            'max_amount_for_sending':0.001,
+#            'min_transactions':5,
+#            'max_transactions':10,
+#            'explorer_url':'https://testnet.snowtrace.io',
+#            'explorer_txn_path':'tx',
+#            }
+#
+
+#__all__ = ['polygon_testnet', 'bsc_testnet', 'avalanche_testnet']
